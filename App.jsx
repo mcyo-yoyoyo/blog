4ac28e8bc1e3f2163c8ffee7aa11a2cdff620a94
mcyo-yoyoyo <ly-fighting@163.com>
2025-12-09 11:40:07 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { initializeApp } from 'firebase/app';
 import { 
   getAuth, 
